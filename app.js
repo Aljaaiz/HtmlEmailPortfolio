9d84c77215145287a8b974b697f65b7acb7dc0a2
Aljaaiz <aljaaiz1@gmail.com>
2021-11-26 17:18:38 +0400

    adding jS functionality

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let hamburger = document.getElementById("hamburger");
 let mobile_nav = document.querySelector(".mobile-nav");
 let close = document.getElementById("close-hamburger");
+let navItem = document.querySelectorAll(".nav-list-item");
 
 hamburger.addEventListener("click", function () {
   mobile_nav.classList.toggle("active");
 });
 close.addEventListener("click", function () {
   mobile_nav.classList.remove("active");
+});
+navItem.forEach(function (item) {
+  item.addEventListener("click", function () {
+    mobile_nav.classList.remove("active");
+  });
 });
